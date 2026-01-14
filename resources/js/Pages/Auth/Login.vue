5bfbc81cf4d42bbe003a45c71ff578b74890d635
Niwarthana00 <sathyanjali00@gmail.com>
2026-01-14 17:23:42 +0530

    Removed Forgot Password link from Login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -54,11 +54,7 @@ const submit = () => {
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
-                <Link :href="route('register')" class="ml-4 underline text-sm text-gray-600 hover:text-gray-900">
+                <Link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900">
                     Register
                 </Link>
 
