5d24928873aa890fcc40ce6d7a391bcf6fbe0722
Niwarthana00 <sathyanjali00@gmail.com>
2026-01-14 19:08:59 +0530

    Fix: Restore header slot for detail pages

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -105,6 +105,13 @@ const showingNavigationDropdown = ref(false);
                 </div>
             </nav>
 
+            <!-- Page Heading -->
+            <header class="bg-gray-800 shadow border-b border-gray-700" v-if="$slots.header">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <slot name="header" />
+                </div>
+            </header>
+
             <!-- Page Content -->
             <main>
                 <div class="py-6">
